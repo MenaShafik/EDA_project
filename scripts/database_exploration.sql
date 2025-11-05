e4cb5904b07efbc72d1adbd4478f1d12347d696a
mena shafik <126687803+MenaShafik@users.noreply.github.com>
2025-11-05 23:49:24 +0200

    Clean up database exploration SQL script
    Refactor SQL queries for database exploration and analysis.

--- scripts/database_exploration.sql
+++ scripts/database_exploration.sql
@@ -1,211 +1,173 @@
-select *
-from INFORMATION_SCHEMA.TABLES
+/*
+===================
+database exploration
+===================
+*/
 
------------------------------------
-select *
-from INFORMATION_SCHEMA.COLUMNS
+--explore all objects in the database
+select * from INFORMATION_SCHEMA.TABLES
+
+
+
+--explore all columns in the database
+
+select * from INFORMATION_SCHEMA.COLUMNS
 where TABLE_NAME = 'dim_customers'
 
+/*
+===================
+dims exploration
+===================
+*/
+--explore all countries our customer come from
 
---------------------------------------------
-select distinct category_name,subcategory,product_name
-from
-gold.dim_products
+select distinct country from gold.dim_customers
 
-----------------------------------------------------
-select min(order_date) first_date,
-	   max(order_date) last_date,
-	   DATEDIFF(year,MIN(order_date),max(order_date)) as range_year
+--explore all cat 
+select distinct category,subcategory,product_name
+from gold.dim_products
+order by 1,2,3
+
+/*
+===================
+date exploration
+===================
+*/
+--first_order & first_order & order_range_date
+select MIN(order_date) first_order,
+ max(order_date) last_order,
+datediff(YEAR ,MIN(order_date), max(order_date)) order_range_date
 from gold.fact_sales
 
-select MAX(BDATE),
-DATEDIFF(YEAR,MAX(BDATE),GETDATE())
+--find youngest & oldest customer
+
+select min(birthdate),
+max(birthdate),
+datediff(year,min(birthdate),GETDATE()),
+datediff(year,max(birthdate),GETDATE())
 from gold.dim_customers
 
+/*
+===================
+measures exploration
+===================
+*/
+--find the total sales
 
-SELECT SUM(sales_amount)
-FROM gold.fact_sales
-
-select count(quantity)
+select sum(sales_amount) as total_sales
 from gold.fact_sales
 
-select avg(price)
+--find how many items are sold
+select sum(quantity)
 from gold.fact_sales
 
-select gender,
-count(customer_key) as total_customer
+--fid the average selling price
+select AVG(price) 
+from gold.fact_sales
+--find the total num of orders
+select count(distinct order_number)
+from gold.fact_sales
+
+--find the total num of products
+select count(distinct product_number)
+from gold.dim_products
+--find the total num of customers
+select count( customer_number)
+from gold.dim_customers
+--find the total num of customers that has placed an order
+
+select count(distinct customer_number)
+from gold.dim_customers
+
+/*
+===================
+magnitude exploration
+===================
+*/
+--find total customers by countries ?
+select distinct country,
+count(customer_key) as total_customer_by_gender
+from gold.dim_customers
+group by country
+
+--find total customers by gender ?
+select distinct gender,
+count(customer_key) as total_customer_by_gender
 from gold.dim_customers
 group by gender
-order by total_customer desc
-
-
-select pd.category_name,
-SUM(sls.sales_amount) as total_sales
-from gold.fact_sales as sls
-left join gold.dim_products as pd
-on pd.product_key = sls.product_key
-group by pd.category_name
-order by total_sales desc
-
-
-
-
-
-select 
-YEAR(order_date) as order_date,
-sum(sales_amount),
-COUNT(distinct customer_key) as total_customer,
-count(quantity) as total_quantity
-from gold.fact_sales
-where order_date is not null
-group by YEAR(order_date)
-order by sum(sales_amount) desc
-
-
-
-
-
-
-select datedi(MONTH,order_date) AS order_date,
-sum(sales_amount)
-from gold.fact_sales
-group by DATETRUNC(MONTH,order_date)
-
-
-------------------------------------
-
-
-
-WITH YEARLY_PRODUCT_SALES AS(
-SELECT YEAR(F.order_date) AS order_YEAR,
-P.product_name,
-SUM(F.sales_amount) AS CURRENT_SALES
-FROM gold.fact_sales AS F
-LEFT JOIN gold.dim_products AS P
-ON P.product_key  = F.product_key
-WHERE order_date IS NOT NULL
-GROUP BY YEAR(F.order_date),P.product_name
-)
-
-SELECT order_YEAR,
-product_name,
-CURRENT_SALES,
-AVG(CURRENT_SALES)OVER(PARTITION BY product_name) AS AVG_SALES,
-CURRENT_SALES - AVG(CURRENT_SALES)OVER(PARTITION BY product_name) AS PERFORMENCE_SALES,
-CASE WHEN CURRENT_SALES - AVG(CURRENT_SALES)OVER(PARTITION BY product_name) > 0 THEN 'Above average'
-	 WHEN CURRENT_SALES - AVG(CURRENT_SALES)OVER(PARTITION BY product_name) < 0 THEN 'below average'
-else 'average'
-end as flag
-FROM YEARLY_PRODUCT_SALES
-ORDER BY product_name,order_YEAR
-
-
-with category_sales as
-(
-select p.category,
-SUM(f.sales_amount) as total_sales
-from gold.fact_sales f
-left join gold.dim_products p
-on f.product_key = p.product_key
-group by p.category
-)
+--find total products by category ?
 select category,
-total_sales,
-CONCAT(ROUND(CAST(total_sales as float) * 100 / SUM(total_sales)over(),1),' %') as overall_sales
-from category_sales
-
-
------------------------------
-with seg as (
-select product_key,
-product_name,
-cost,
-case when cost < 100 then 'below 100'
- when cost between 100 and 500 then '100-500'
- when cost between 500 and 1000 then '500-1000'
- else 'above 1000'
- end as cost_range
+COUNT(product_key) as total_sales_by_cat
 from gold.dim_products
-)
+group by category
+--avg cost in each category ?
+SELECT category,
+AVG(cost) as avg_sales_by_cat
+FROM gold.dim_products
+group by category
+--the total revenue is generated for each category ?
+select p.category,
+SUM(fs.sales_amount) as sales_amount
+from gold.fact_sales as fs
+left join gold.dim_products as p
+on p.product_key = fs.product_key
+group by p.category 
 
-select cost_range,
-COUNT(product_key) as total_p
-from seg
-group by cost_range
-
-----------------------
-with spend as (
+--find total revenue is generated by each customer ?
 select c.customer_key,
-SUM(f.sales_amount) AS sales_total,
-min(order_date) as first_order,
-min(order_date) as last_order,
-DATEDIFF(month,min(order_date),min(order_date)) as lifespan
-from gold.fact_sales f
-left join gold.dim_customers c
-on f.customer_key = c.customer_key
-group by c.customer_key
-order by DATEDIFF(month,min(order_date),min(order_date)) desc
-)
-select customer_key,
-case when lifespan >= 12 and sales_total > 5000 then 'VIP'
-WHEN lifespan >= 12 and sales_total <= 5000 then 'regular'
-else 'new'
-end customer_seg
-from spend
-
-with base_query as(
-select f.order_number,
-f.product_key,
-f.order_date,
-f.sales_amount,
-f.quantity,
-c.customer_key,c.customer_number,
 c.first_name,
 c.last_name,
-CONCAT(c.first_name,' ',c.last_name) as customer_name,
-c.birthdate,
-DATEDIFF(YEAR,c.birthdate,GETDATE()) as age
-from gold.fact_sales f
-left join gold.dim_customers c
-on f.customer_key = c.customer_key
-where order_date is not null
-),
-customer_aggregation as (
-select 
-customer_key,
-customer_number,
-customer_name,
-age,
-COUNT(sales_amount) as total_sales,
-sum(quantity) as total_quantity,
-count(distinct product_key) as total_products,
-max(order_date) as last_order_date,
-DATEDIFF(month,min(order_date),max(order_date)) as lifespan
-from base_query
-group by customer_key,
-customer_number,
-customer_name,
-age
-)
-select customer_key,
-customer_number,
-customer_name,
-age,
-case when age < 20  then  'under 20 '
-	when age between 20 and 29 then '20-29'
-	when age between 30 and 39 then '30-39'
-	when age between 40 and 49 then '40-49'
-	else '50 and above'
-	end as age_group,
-case when lifespan >= 12 and total_sales > 5000 then 'VIP'
-	 WHEN lifespan >= 12 and total_sales <= 5000 then 'regular'
-else 'new'
-end customer_segtotal_orders,
-total_sales,
-total_quantity,
-total_products,
-DATEDIFF(month,last_order_date,GETDATE()) as recancy,
+SUM(fs.sales_amount) as sales_amount
+from gold.fact_sales as fs
+left join gold.dim_customers as c
+on c.customer_key = fs.customer_key
+group by c.customer_key,
+c.first_name,
+c.last_name
+--what od the distribution of sold items accross countries ?
 
-last_order_date,
-lifespan
-from customer_aggregation
+select c.country,
+SUM(fs.sales_amount) as sales_amount
+from gold.fact_sales as fs
+left join gold.dim_customers as c
+on c.customer_key = fs.customer_key
+group by c.country
+
+/*
+===================
+Ranking exploration
+===================
+*/
+--generate the top 5 products that have high revenue
+
+select top (5) p.product_name,
+SUM(fs.sales_amount) as sales_amount
+from gold.fact_sales as fs
+left join gold.dim_products as p
+on p.product_key = fs.product_key
+group by p.product_name
+order by sales_amount desc
+
+--generate the worst 5 products that have high revenue
+
+select top (5) p.product_name,
+SUM(fs.sales_amount) as sales_amount
+from gold.fact_sales as fs
+left join gold.dim_products as p
+on p.product_key = fs.product_key
+group by p.product_name
+order by sales_amount
+--------------------------------------------------
+select product_name,
+sales_amount
+from (
+select p.product_name,
+SUM(fs.sales_amount) as sales_amount,
+ROW_NUMBER()over(order by SUM(fs.sales_amount)) as rank
+from gold.fact_sales as fs
+left join gold.dim_products as p
+on p.product_key = fs.product_key
+group by p.product_name
+)t
+where rank <=5
+
